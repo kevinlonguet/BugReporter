06b88b74b4113768e0e0321407c3f86c7370f256
Maxime AUBLET <maximeaublet@gmail.com>
2018-11-27 16:12:37 +0100

    user crud

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -24,29 +24,6 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="user_new", methods="GET|POST")
-     */
-    public function new(Request $request): Response
-    {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
-
-            return $this->redirectToRoute('user_index');
-        }
-
-        return $this->render('user/new.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
      * @Route("/{id}", name="user_show", methods="GET")
      */
     public function show(User $user): Response
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -14,7 +14,6 @@ class UserType extends AbstractType
         $builder
             ->add('email')
             ->add('roles')
-            ->add('password')
             ->add('firstname')
             ->add('lastname')
             ->add('id_team')
